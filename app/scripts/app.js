5e127485827e414d5bf9c65f6d2042d0dd3fe461
Eric Mosher <emoshmosh@gmail.com>
2014-04-30 20:02:12 -0400

    Changing some boilerplate from the scaffolding

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,15 @@
 'use strict';
 
 angular.module('ozpWebtopApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+]).config(function ($routeProvider) {
+    $routeProvider.when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      })
-      .otherwise({
+    }).otherwise({
         redirectTo: '/'
-      });
-  });
+    });
+});

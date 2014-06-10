01066eceb8872d3cedcd259370f1afa1d55a3edc
Eric Mosher <emoshmosh@gmail.com>
2014-06-10 17:11:06 -0400

    Remove unused controllers

--- app/scripts/controllers/frame.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('ozpWebtopApp')
-    .controller('FrameController', function ($scope) {
-
-    });
--- app/scripts/controllers/main.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-// Placeholder controller, currently unused
-angular.module('ozpWebtopApp.controllers').controller('MainCtrl', function () {});

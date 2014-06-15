6f920321f21abe8db8dfa3b51096893da95149c5
Eric Mosher <emoshmosh@gmail.com>
2014-06-15 19:06:07 -0400

    Wired up icons to create a button on the bottom toolbar  - Create icon demo within desktop view  - Change and adde CSS to show icons correctly  - Add the ability to add a button to the bottom toolbar upon an icon click  - Closes #14

--- app/scripts/controllers/desktop.js
+++ app/scripts/controllers/desktop.js
@@ -1,9 +1,17 @@
 'use strict';
 
 angular.module('ozpWebtopApp.controllers')
+
     .controller('DesktopController', function ($scope, WorkspaceState) {
+
         // GET the state of the frames
-        WorkspaceState.getStateFile('frames').then(function(data) {
+        WorkspaceState.getStateFile('frames').then(function (data) {
             $scope.frames = data.frames;
         });
+
+        // GET the state of the icons
+        WorkspaceState.getStateFile('icons').then(function (data) {
+            $scope.icons = data.icons;
+        });
+
     });
--- app/scripts/directives/ozpicon.js
+++ app/scripts/directives/ozpicon.js
@@ -8,10 +8,29 @@
  * @constructor
  */
 angular.module('ozpWebtopApp.directives')
+
     .directive('ozpIcon', function () {
+
         return {
+
             replace: true,
+
             templateUrl: 'templates/ozpicon.html',
-            restrict: 'E'
+
+            restrict: 'E',
+
+            // Controller is a placeholder, this may need to be removed/refactored...
+            controller: function ($scope) {
+
+                $scope.handleIconClick = function (icon) {
+
+                    // Fire a scope event to notify other scopes of the icon click
+                    $scope.$emit('iconClick', icon);
+
+                };
+
+            }
+
         };
+
     });

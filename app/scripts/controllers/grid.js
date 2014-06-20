e3d11b63086ffc6bfc81d43cfba57dfca68034c3
Eric Mosher <emoshmosh@gmail.com>
2014-06-20 14:10:36 -0400

    Implementing injection of frames into Gridster directive  - Still a WIP, working towards #8  - Additional enhancements to the Gridster Item directive  - Now loading a mock state into the Gridster view

--- app/scripts/controllers/grid.js
+++ app/scripts/controllers/grid.js
@@ -4,30 +4,12 @@
  *
  */
 angular.module('ozpWebtopApp.controllers')
-    .controller('GridController', function ($scope) {
-        $scope.grid = [{
-            col : 1,
-            row: 1,
-            sizey: 1,
-            sizex: 1,
-            name: "FOO"
-          },{
-            col : 2,
-            row: 1,
-            sizey: 1,
-            sizex: 1,
-            name: "BAR"
-          },{
-            col : 1,
-            row: 2,
-            sizey: 1,
-            sizex: 1,
-            name: "NG"
-          },{
-            col : 2,
-            row: 2,
-            sizey: 1,
-            sizex: 1,
-            name: "Desktop"
-          }];
+
+    .controller('GridController', function ($scope, WorkspaceState) {
+
+        // GET the state of the grid
+        WorkspaceState.getStateFile('tiles').then(function (data) {
+            $scope.grid = data.tiles;
+        });
+
     });
--- app/scripts/directives/gridster.js
+++ app/scripts/directives/gridster.js
@@ -4,41 +4,46 @@
  *
  */
 angular.module('ozpWebtopApp.directives')
-    .directive('gridster', ['$timeout', '$parse',
-    function ($timeout, $parse, gridsterConfig) {
+
+    .directive('ozpGridster', function ($timeout, $parse) {
 
         var gridsterConfig = {
             widget_margins: [5, 5],
+            widget_base_dimensions: [200, 200],
             widget_selector: 'li',
-            max_cols: 2
+            min_cols: 5,
+            draggable: {
+                handle: 'div.ozp-chrome'
+            }
         };
 
         return {
+
             restrict: 'AE',
+
             scope: {
                 ngModel: '=',
                 options: '=',
                 updated: '&'
             },
+
             require: '^ngModel',
-            template: '<ul><li class="gs-w" ng-repeat="item in ngModel" ng-model="item"' +
-                            'data-col="{{item.col}}" data-row="{{item.row}}"' +
-                                'data-sizex="{{item.sizex}}" data-sizey="{{item.sizey}}">' +
-                        '{{item.name}}</li></ul>',
-            link: function ($scope, $element, $attributes, $controller) {
+
+            templateUrl: 'templates/gridster.html',
+
+            link: function (scope, element, attributes, controller) {
 
                 var options = angular.extend(gridsterConfig,
-                    $scope.$eval($attributes.gridsterOptions));
+                    scope.$eval(attributes.gridsterOptions));
 
                 var gridster;
                 $timeout(function() {
-                    var el = $element.find('ul')
+                    var el = element.find('ul')
                         .gridster(options);
                     gridster = el.data('gridster');
                 });
 
                 var attachModel = function (newVal, oldVal) {
-
                     if (!!newVal) {
                         if(newVal.length == oldVal.length) return;
 
@@ -55,8 +60,61 @@ angular.module('ozpWebtopApp.directives')
                     }
                 };
 
-                return $scope.$watch('ngModel', attachModel, true);
+                return scope.$watch('ngModel', attachModel, true);
+            },
+
+            controller: function ($scope, $element, $attrs) {
+                // TODO
             }
         };
 
-    }]);
+    })
+
+    .directive('gridsterItem', function ($compile, $http, compareUrl) {
+
+        var getTemplate = function (sameOrigin) {
+            var template = '';
+
+            // If different origin, use an iframe template
+            if (!sameOrigin) {
+                template = 'templates/managediframe.html';
+            }
+            // otherwise, use a 'frame' (div) template
+            else {
+                template = 'templates/managedframe.html';
+            }
+            return template;
+        };
+
+        return {
+
+            replace: true,
+
+            restrict: 'AE',
+
+            scope: {
+                frame: '='
+            },
+
+            link: function (scope, element, attributes, controller) {
+
+                // Is the origin the same as the webtop?
+                var origin = compareUrl(scope.frame.url);
+
+                // Instead of templateUrl, use $http to load one of two templates
+                $http.get(getTemplate(origin)).then(function(response) {
+                    element.html($compile(response.data)(scope));
+                });
+
+                // TODO: make these dynamic
+                scope.styles = {
+                    'height': 181,
+                    'width': 150
+                };
+
+            }
+
+        };
+
+    });
+

68cb7b5cc4fca2ac044a07337087087e21ee145e
Clark Jefcoat <clark.j.jefcoat@intelink.gov>
2014-06-27 16:05:36 -0400

    Simple passing controller tests

--- test/spec/controllers/iframe.js
+++ test/spec/controllers/iframe.js
@@ -11,12 +11,14 @@ describe('Controller: IframeCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    IframeCtrl = $controller('IframeCtrl', {
+    scope.frame = {}
+    scope.frame.url = 'http://localhost:9000/test.html'
+    IframeCtrl = $controller('IframeController', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should put a trusted URL in the scope', function () {
+    expect(scope.frame.trustedUrl).not.toBeNull();
   });
 });

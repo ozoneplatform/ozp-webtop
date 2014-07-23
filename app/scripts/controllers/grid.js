56f4ab3d4fb80117aeca344d72fb988f27c373e9
Eric Mosher <emoshmosh@gmail.com>
2014-07-23 08:28:26 -0400

    Tweaks in angular-gridster config

--- app/scripts/controllers/grid.js
+++ app/scripts/controllers/grid.js
@@ -33,18 +33,18 @@ angular.module('ozpWebtopApp.controllers')
             margins: [5, 5], // the pixel distance between each widget
             isMobile: false, // stacks the grid items if true
             minColumns: 1, // the minimum columns the grid must have
-            minRows: 5, // the minimum height of the grid, in rows
+            minRows: 2, // the minimum height of the grid, in rows
             maxRows: 100,
             resizable: {
                enabled: true,
-               handles: 'se',
+               handles: 'n, e, s, w, ne, se, sw, nw',
                start: function(event, uiWidget, $element) {}, // optional callback fired when resize is started,
                resize: function(event, uiWidget, $element) { console.log(event); console.log(uiWidget); console.log($element);}, // optional callback fired when item is resized,
                stop: function(event, uiWidget, $element) {} // optional callback fired when item is finished resizing
             },
             draggable: {
                enabled: true, // whether dragging items is supported
-               // handle: '.my-class', // optional selector for resize handle
+               handle: 'div.ozp-chrome, div.ozp-chrome > .chrome-icon, div.ozp-chrome > .chrome-name', // optional selector for resize handle
                start: function(event, uiWidget, $element) {}, // optional callback fired when drag is started,
                drag: function(event, uiWidget, $element) {}, // optional callback fired when item is moved,
                stop: function(event, uiWidget, $element) {} // optional callback fired when item is finished dragging

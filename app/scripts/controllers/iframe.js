c814e2affc5afc4dac4e78e9c593d2e9cb4e48cf
Alan Ward <arw180@gmail.com>
2014-07-30 08:13:51 -0400

    more changes from webtop-ngboilerplate

--- app/scripts/controllers/iframe.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('ozpWebtopApp.controllers')
-    .controller('IframeController', function ($scope, $sce) {
-        $scope.frame.trustedUrl = $sce.trustAsResourceUrl($scope.frame.url);
-    });

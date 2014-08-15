db9b35c6890db9d975761831c5658058761d8010
Alan Ward <arw180@gmail.com>
2014-08-15 14:16:36 -0400

    issue #118 - creating new dashboardChangeMonitor service that broadcasts messages when the dashboard or dashboard layout changes

--- src/app/appToolbar/appToolbar.js
+++ src/app/appToolbar/appToolbar.js
@@ -1,47 +1,46 @@
 'use strict';
 
 angular.module( 'ozpWebtopApp.appToolbar')
-.controller('appToolbarCtrl', function($scope, $rootScope, $location, $state, marketplaceApi, dashboardApi) {
+.controller('appToolbarCtrl', function($scope, $rootScope, $state,
+                                       marketplaceApi, dashboardApi,
+                                       dashboardChangeMonitor) {
 
-  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  //                      Data from services
-  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  $rootScope.$watch('activeFrames', function () {
+    $scope.currentDashboardIndex = '0';
 
-    if ($rootScope.activeFrames) {
-      $scope.myPinnedApps = $rootScope.activeFrames;
-    }
-  });
+    $rootScope.$watch('activeFrames', function () {
 
-   $scope.$watch(function() {
-      return $location.path();
-    }, function() {
-     // TODO: use a regex or something less hacky
-     $scope.currentDashboardIndex = $location.path().slice(-1);
-   });
+      if ($rootScope.activeFrames) {
+        $scope.myPinnedApps = $rootScope.activeFrames;
+      }
+    });
 
-   $scope.maximizeFrame = function(e) {
-     if(e.isMinimized === true){
-       e.isMinimized = false;
-     }
-   };
+    // register to receive notifications if dashboard changes
+    dashboardChangeMonitor.run();
 
-  $scope.myApps = marketplaceApi.getAllApps();
+    $scope.$on('dashboardChange', function(event, dashboardChange) {
+      $scope.currentDashboardIndex = dashboardChange.dashboardIndex;
+    });
 
-  $scope.appClicked = function(app) {
-    // check if the app is already on the current dashboard
-    var isOnDashboard = dashboardApi.isAppOnDashboard($scope.currentDashboardIndex, app.uuid);
-    if (isOnDashboard) {
-      alert('This application is already on your dashboard');
-    } else {
-      // add this app to the dashboard
-      // TODO: use message broadcast to get grid max rows and grid max cols
-      dashboardApi.addApplication($scope.currentDashboardIndex, app.uuid, 10);
-      // reload this dashboard
-      $state.go($state.$current, null, { reload: true });
+     $scope.maximizeFrame = function(e) {
+       if(e.isMinimized === true){
+         e.isMinimized = false;
+       }
+     };
 
-    }
+    $scope.myApps = marketplaceApi.getAllApps();
 
-  };
-
+    $scope.appClicked = function(app) {
+      // check if the app is already on the current dashboard
+      var isOnDashboard = dashboardApi.isAppOnDashboard(
+        $scope.currentDashboardIndex, app.uuid);
+      if (isOnDashboard) {
+        alert('This application is already on your dashboard');
+      } else {
+        // add this app to the dashboard
+        // TODO: use message broadcast to get grid max rows and grid max cols
+        dashboardApi.addApplication($scope.currentDashboardIndex, app.uuid, 10);
+        // reload this dashboard
+        $state.go($state.$current, null, { reload: true });
+      }
+    };
   });

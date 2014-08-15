330faaa13b0e74757efc776a414fffe04254d843
Alan Ward <arw180@gmail.com>
2014-08-15 15:26:42 -0400

    issue #118 - adding unit test for dashboardChangeMonitor

---
+++ src/app/general/services/dashboardChangeMonitor.spec.js
@@ -0,0 +1,59 @@
+'use strict';
+
+describe('Service: dashboardChangeMonitor', function () {
+  // load the service's module
+  beforeEach(module('ozpWebtopApp'));
+
+  // instantiate service
+  var $rootScope, $location, dashboardChangeMonitor;
+  beforeEach(inject(function (_$rootScope_, _$location_, _dashboardChangeMonitor_) {
+    $rootScope = _$rootScope_;
+    $location = _$location_;
+    dashboardChangeMonitor = _dashboardChangeMonitor_;
+    spyOn($rootScope, '$broadcast').andCallThrough();
+  }));
+
+  it('has a run method', function() {
+    expect(dashboardChangeMonitor.run).toBeDefined();
+  });
+
+  it('exposes the layout property', function() {
+    dashboardChangeMonitor.run();
+    $location.path('/grid/0');
+    $rootScope.$apply();
+    expect(dashboardChangeMonitor.layout).toBe('grid');
+
+    $location.path('/desktop/5');
+    $rootScope.$apply();
+    expect(dashboardChangeMonitor.layout).toBe('desktop');
+  });
+
+  it('exposes the dashboardIndex property', function() {
+    dashboardChangeMonitor.run();
+    $location.path('/grid/0');
+    $rootScope.$apply();
+    expect(dashboardChangeMonitor.dashboardIndex).toBe('0');
+
+    $location.path('/grid/0');
+    $rootScope.$apply();
+    expect(dashboardChangeMonitor.dashboardIndex).toBe('0');
+  });
+
+  it('sends messages when the url changes', function() {
+    dashboardChangeMonitor.run();
+
+    $location.path('/grid/0');
+    $rootScope.$apply();
+    var obj = {'layout': 'grid', 'dashboardIndex': '0'};
+    expect($rootScope.$broadcast).toHaveBeenCalledWith('dashboardChange', obj);
+  });
+
+  it('supports double-digit dashboard indexes', function() {
+    dashboardChangeMonitor.run();
+    $location.path('/desktop/12');
+    $rootScope.$apply();
+    var obj = {'layout': 'desktop', 'dashboardIndex': '12'};
+    expect($rootScope.$broadcast).toHaveBeenCalledWith('dashboardChange', obj);
+  });
+
+});

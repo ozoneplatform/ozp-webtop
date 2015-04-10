1a2204b735c6eecc2edadbbd192eceb7000fcd3d
Alan Ward <arw180@gmail.com>
2015-04-10 07:27:34 -0400

    chore(OzoneConfig.js): make API_URL the same as HUD and Center
    Note that this will require mods to the dev-tools scripts that generate
    the OzoneConfig.js file
    
    Issue #515

--- src/OzoneConfig.js
+++ src/OzoneConfig.js
@@ -1,8 +1,8 @@
 window.OzoneConfig = {
-    'API_URL': 'https://www.owfgoss.org/ng/dev/mp/api',
+    'API_URL': 'https://www.owfgoss.org/ng/dev/mp',
     'IWC_URL': 'https://ozone-development.github.io/iwc',
     'CENTER_URL': 'https://ozone-development.github.io/ozp-center',
     'HUD_URL': 'https://ozone-development.github.io/ozp-hud',
     'WEBTOP_URL': 'https://ozone-development.github.io/ozp-webtop',
-    'METRICS_URL': '/path/to/metrics'  
+    'METRICS_URL': '/path/to/metrics'
 };
